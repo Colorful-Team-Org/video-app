5b97127e09656edff73660252407b0ebd635243d
Bartlomiej Mirynski <1122641+bkmy@users.noreply.github.com>
2022-09-06 23:15:00 +0200

    - Cancel Upload Button & Modal update. - Cleanup of unused ButtonUpload component

--- src/features/wistia/components/CancelUpload.tsx
+++ src/features/wistia/components/CancelUpload.tsx
@@ -1,40 +1,62 @@
-import {IconButton, ModalConfirm, Text, Tooltip} from '@contentful/f36-components';
+import {Box, IconButton, ModalLauncher, ModalConfirm, Text, Tooltip} from '@contentful/f36-components';
 import {CloseTrimmedIcon} from '@contentful/f36-icons';
+import {Notification} from "@contentful/f36-notification";
 import React from "react";
 
 const CancelUpload = () => {
 
-    const [isShown, setIsShown] = React.useState(false);
+    const handleCancel = () => {
+        ModalLauncher.open(({isShown, onClose}) => {
+            return (
+                <ModalConfirm
+                    title="Cancel video upload"
+                    intent="negative"
+                    isShown={isShown}
+                    allowHeightOverflow={true}
+                    onCancel={() => {
+                        onClose('Video upload continues...')
+                    }}
+                    onConfirm={() => {
+                        window.wistiaUploader.cancel();
+                        onClose('Video upload cancelled')
+                    }}
+                    confirmLabel="Cancel upload anyway"
+                    cancelLabel="Continue uploading">
+                    <Text>Do you really want to cancel uploading this video?</Text>
+                </ModalConfirm>
+            );
+        }).then((result) => {
+            if (result.includes('cancelled')) {
+                Notification.warning(result, {
+                    cta: {
+                        label: 'Try again',
+                        textLinkProps: {
+                            variant: 'primary',
+                            onClick: () => {
+                                Notification.closeAll();
+                                window.location.reload();
+                            }
+                        },
+                    },
+                });
+            } else {
+                Notification.success(result);
+            }
+        });
+    }
 
     return (
-        <>
-
-            <Tooltip content="Cancel upload">
+        <Box style={{marginLeft: 'auto'}}>
+            <Tooltip content="Cancel upload" placement="top-end">
                 <IconButton
                     aria-label="Cancel upload"
                     icon={<CloseTrimmedIcon/>}
-                    variant="transparent"
-                    size="medium"
-                    onClick={() => setIsShown(true)}
+                    variant="negative"
+                    size="small"
+                    onClick={handleCancel}
                 />
             </Tooltip>
-
-            <ModalConfirm
-                title="Cancel video upload"
-                intent="negative"
-                isShown={isShown}
-                onCancel={() => {
-                    setIsShown(false);
-                }}
-                onConfirm={() => {
-                    setIsShown(false);
-                    window.wistiaUploader.cancel();
-                }}
-                confirmLabel="Cancel upload anyway"
-                cancelLabel="Continue uploading">
-                <Text>Do you really want to cancel uploading this video?</Text>
-            </ModalConfirm>
-        </>
+        </Box>
     );
 }
 
