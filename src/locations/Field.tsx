67b8ac6e8ee3e4f68cc3cb26cdde4e8ace7ee396
Bartlomiej Mirynski <1122641+bkmy@users.noreply.github.com>
2022-09-06 23:20:01 +0200

    - Modal confirm for Remove video feature added, video edit features UI update.

--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -1,7 +1,17 @@
 import React, {useEffect} from 'react';
-import {Stack, Box, Flex, IconButton, Tooltip} from '@contentful/f36-components';
-import {DeleteIcon, AssetIcon, CycleIcon} from '@contentful/f36-icons';
-import tokens from "@contentful/f36-tokens";
+import {
+    Stack,
+    Box,
+    Flex,
+    Tooltip,
+    Button,
+    ButtonGroup,
+    ModalLauncher,
+    ModalConfirm,
+    Text
+} from '@contentful/f36-components';
+import {DeleteIcon, AssetIcon, CycleIcon, InfoCircleIcon} from '@contentful/f36-icons';
+// import tokens from "@contentful/f36-tokens";
 import {FieldExtensionSDK} from '@contentful/app-sdk';
 import { /* useCMA, */useFieldValue, useSDK} from '@contentful/react-apps-toolkit';
 import Wistia from '../features/wistia/Wistia';
@@ -33,68 +43,97 @@ const Field = () => {
         }
 
         await setMedia(result);
-
-        //TODO: remove console.log
-        console.log(`File picker media`, media)
     }
 
+    const handleRemove= () => {
+        ModalLauncher.open(({isShown, onClose}) => {
+            return (
+                <ModalConfirm
+                    title="Remove video data"
+                    intent="negative"
+                    isShown={isShown}
+                    allowHeightOverflow={true}
+                    onCancel={() => {
+                        onClose('Video removal cancelled');
+                    }}
+                    onConfirm={() => {
+                        setMedia(undefined);
+                        onClose('Video data removed, this action cannot be undone. Note, the file is still available in your Wistia project.')
+                    }}
+                    confirmLabel="Remove the video data"
+                    cancelLabel="Keep the video">
+                    <Text>Do you really want to remove this video data?</Text>
+                </ModalConfirm>
+            );
+        }).then((result) => {
+            if (result.includes('cancelled')) {
+                Notification.warning(result);
+            } else {
+                Notification.success(result);
+                setTimeout(() => {
+                    Notification.closeAll();
+                    window.location.reload();
+                },3000);
+            }
+        });
+    }
+
+    const InfoIconTooltip = ({note, id}: any) => {
+        return (
+            <Tooltip content={note} id={id}>
+                <Flex alignItems="center">
+                    <InfoCircleIcon size="small"/>
+                </Flex>
+            </Tooltip>
+        )
+    }
 
     return (
         <Box>
-            <Wistia viewVideosList={viewVideosList}/>
+            <Wistia viewVideosList={viewVideosList} />
 
             {media !== undefined &&
                 <Preview media={media}/>
             }
-            {media !== undefined &&
-                <Box
-                    style={{
-                        margin: '1rem auto',
-                    }}
-                >
-                    <Flex
-                        justifyContent="center"
-                        style={{
-                            backgroundColor: tokens.gray200,
-                            borderRadius: tokens.borderRadiusMedium,
-                            maxWidth: '30%',
-                            margin: 'auto',
-                        }}
-                    >
-                        <Stack margin="spacingXs">
-                            <Tooltip content="Replace Video File">
-                                <IconButton
-                                    aria-label="Replace Video File"
-                                    icon={<CycleIcon/>}
-                                    variant="transparent"
-                                    size="small"
-                                    onClick={() => console.log('Work in progress...')}
-                                />
-                            </Tooltip>
-                            <Tooltip content="Set Thumbnail">
-                                <IconButton
-                                    aria-label="Set Thumbnail"
-                                    icon={<AssetIcon/>}
-                                    variant="transparent"
-                                    size="small"
-                                    onClick={() => console.log('Work in progress...')}
-                                />
-                            </Tooltip>
-                            <Tooltip content="Remove Video">
-                                <IconButton
-                                    aria-label="Remove Video"
-                                    icon={<DeleteIcon/>}
-                                    variant="transparent"
-                                    size="small"
-                                    onClick={() => {
-                                        setMedia(undefined)
-                                        Notification.warning('Media data removed, please update access token permissions in order to remove it from Wistia project.')
-                                    }}
-                                />
-                            </Tooltip>
-                        </Stack>
-                    </Flex>
-                </Box>}
+            {media !== undefined && <Box>
+                <Stack
+                    spacing="spacingM"
+                    justifyContent="space-evenly">
+                    <ButtonGroup variant="spaced" spacing="spacing2Xs">
+                        <Button
+                            aria-label="Replace Video File"
+                            startIcon={<CycleIcon/>}
+                            variant="transparent"
+                            isDisabled={true}>
+                            Replace Video File
+                        </Button>
+                        <InfoIconTooltip note="When a media is replaced, the app overwrites the original video file but keeps all the other data -- name, description, thumbnail, stats -- unchanged. Use this feature to update a video with a slight editing tweak."
+                                         id="replace-video-file"/>
+                    </ButtonGroup>
+                    <ButtonGroup variant="spaced" spacing="spacing2Xs">
+                        <Button
+                            aria-label="Set Thumbnail"
+                            startIcon={<AssetIcon/>}
+                            variant="transparent"
+                            onClick={() => console.log('Work in progress...')}>
+                            Set Thumbnail</Button>
+                        <InfoIconTooltip
+                            note="The thumbnail is what viewers see before they press play. By default, Wistia selects the middle frame of the video. If you want to replace it with an alternative shot, pause the video on the desired frame and hit this link."
+                            id="set-thumbnail"/>
+                    </ButtonGroup>
+                    <ButtonGroup variant="spaced" spacing="spacing2Xs">
+                        <Button
+                            aria-label="Remove Video"
+                            startIcon={<DeleteIcon/>}
+                            variant="transparent"
+                            onClick={() => handleRemove()}>
+                            Remove Video</Button>
+                        <InfoIconTooltip
+                            note="To remove the attached video or attach a video from Wistia, press on this link. Removing the video, only removes it from Contentful, but it continues to live in Wistia. To remove the video completely, contact your colleague with access to our Wistia account."
+                            id="remove-video"/>
+                    </ButtonGroup>
+                </Stack>
+            </Box>}
         </Box>
     );
 };
