3b025017383789e266e64e8dec006e5d68f9b935
Bartlomiej Mirynski <1122641+bkmy@users.noreply.github.com>
2022-09-12 19:26:26 +0200

    Button retry update.

--- src/features/wistia/components/ButtonRetry.tsx
+++ src/features/wistia/components/ButtonRetry.tsx
@@ -1,19 +1,12 @@
 import {Button} from '@contentful/f36-components';
 import React from "react";
 
-const ButtonRetry = ({progress}: any) => {
-
+const ButtonRetry = () => {
     return (
         <Button
             variant="primary"
-            style={{marginTop: '10px'}}
-            onClick={() => {
-                window.location.reload();
-            }}>
-            {progress === "100% uploaded" || progress === ''
-                ? 'Let\'s try again'
-                : 'Upload a new video'
-            }
+            onClick={() => window.location.reload()}>
+            Upload a new video
         </Button>
     );
 }
