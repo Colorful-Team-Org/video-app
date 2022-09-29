95a286d5342ac32de093856af4771ae86e60a0e5
Bartlomiej Mirynski <1122641+bkmy@users.noreply.github.com>
2022-09-29 22:52:13 +0200

    Cancel video Dialog update (alternative to broken f36 ModalConfirm)

--- src/features/wistia/components/CancelUpload.tsx
+++ src/features/wistia/components/CancelUpload.tsx
@@ -1,35 +1,58 @@
 import {Box, IconButton, ModalLauncher, ModalConfirm, Text, Tooltip} from '@contentful/f36-components';
 import {CloseTrimmedIcon} from '@contentful/f36-icons';
 import React from "react";
+import {useSDK} from "@contentful/react-apps-toolkit";
+
 
 const CancelUpload = () => {
+    const sdk = useSDK();
 
-    const handleCancel = () => {
-        ModalLauncher.open(({isShown, onClose}) => {
-            return (
-                <ModalConfirm
-                    title="Cancel video upload"
-                    intent="negative"
-                    isShown={isShown}
-                    allowHeightOverflow={true}
-                    onCancel={() => {
-                        onClose(() => {
-                            return false;
-                        });
-                    }}
-                    onConfirm={() => {
-                        window.wistiaUploader.cancel();
-                        onClose(() => {
-                            window.location.reload();
-                        });
-                    }}
-                    confirmLabel="Cancel upload anyway"
-                    cancelLabel="Continue uploading">
-                    <Text>Do you really want to cancel uploading this video?</Text>
-                </ModalConfirm>
-            );
-        }).then((result) => result());
-    }
+    const handleCancel= () => {
+        sdk.dialogs.openConfirm({
+            title: 'Cancel video upload',
+            message: 'Do you really want to cancel uploading this video?',
+            intent: 'negative',
+            confirmLabel: 'Cancel upload anyway',
+            cancelLabel: 'Continue uploading',
+            shouldCloseOnEscapePress: true,
+            shouldCloseOnOverlayClick: true,
+        }).then((res) => {
+            if (res === true) {
+                window.wistiaUploader.cancel();
+                window.location.reload();
+            }
+            return;
+
+        });
+    };
+
+    // TODO: Investigate Broken ModalConfirm
+    // const handleCancel = () => {
+    //     ModalLauncher.open(({isShown, onClose}) => {
+    //         return (
+    //             <ModalConfirm
+    //                 title="Cancel video upload"
+    //                 intent="negative"
+    //                 isShown={isShown}
+    //                 allowHeightOverflow={true}
+    //                 onCancel={() => {
+    //                     onClose(() => {
+    //                         return false;
+    //                     });
+    //                 }}
+    //                 onConfirm={() => {
+    //                     window.wistiaUploader.cancel();
+    //                     onClose(() => {
+    //                         window.location.reload();
+    //                     });
+    //                 }}
+    //                 confirmLabel="Cancel upload anyway"
+    //                 cancelLabel="Continue uploading">
+    //                 <Text>Do you really want to cancel uploading this video?</Text>
+    //             </ModalConfirm>
+    //         );
+    //     }).then((result) => result());
+    // };
 
     return (
         <Box style={{marginLeft: 'auto'}}>
