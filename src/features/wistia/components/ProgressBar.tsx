92cd47db0412385c70a82ca688344c47f3227f80
Bartlomiej Mirynski <1122641+bkmy@users.noreply.github.com>
2022-10-11 14:49:55 +0200

    Progress bar UI updates for smoother animation of upload progress.

--- src/features/wistia/components/ProgressBar.tsx
+++ src/features/wistia/components/ProgressBar.tsx
@@ -1,25 +1,20 @@
-import {Box} from '@contentful/f36-components';
 import React from "react";
+import {Box} from '@contentful/f36-components';
+import {cx} from "emotion";
+import {styles} from "./ProgressBar.styles"
 
 const ProgressBar = ({progress}: any) => {
 
     return (
         <Box
             as="div"
-            style={{
-                width: '80%',
-                height: '8x',
-                margin: '1rem auto',
-                backgroundColor: 'rgba(3,111,227,0.2)',
-                borderRadius: '6px',
-            }}>
+            className={cx(styles.progressBar)}
+        >
             <Box
                 as="div"
+                className={cx(styles.progressBar__progress, progress)}
                 style={{
                     width: progress ? progress : '0%',
-                    height: '12px',
-                    backgroundColor: '#036fe3',
-                    borderRadius: '6px',
                 }}>
             </Box>
         </Box>
